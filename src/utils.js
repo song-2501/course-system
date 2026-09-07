c78455701e14f2a5f88660275155278875e2716d
song-2501 <467621965@qq.com>
2026-09-07 14:20:00 +0800

    feat: 编写全局样式与公共常量工具

---
+++ src/utils.js
@@ -0,0 +1,6 @@
+export const WEEK_DAYS = ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+export const PERIODS = ['第1-2节', '第3-4节', '第5-6节', '第7-8节', '第9-10节']
+export const TOTAL_WEEKS = 16
+
+export const weekDayName = (d) => WEEK_DAYS[d]
+export const periodName = (p) => PERIODS[p]
